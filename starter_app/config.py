e3ee0f375e5ac7ada8f215ea276a8fdfa440aab5
Ben Pu <bpu991@gmail.com>
2021-07-08 15:38:36 -0400

    changed config

--- starter_app/config.py
+++ starter_app/config.py
@@ -5,5 +5,7 @@ class Config:
   SQLALCHEMY_TRACK_MODIFICATIONS=True
   # SQLALCHEMY_DATABASE_URI = os.environ.get('HEROKU_POSTGRESQL_PURPLE_URL')
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+  if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
   SQLALCHEMY_ECHO=True
   
